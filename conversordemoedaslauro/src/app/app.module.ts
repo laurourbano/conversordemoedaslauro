183307697de56da08f417b3a6ad8d6448c548780
laurourbano <74082012+laurourbano@users.noreply.github.com>
2023-01-19 22:33:59 -0300

    corrigindo projeto

--- conversordemoedaslauro/src/app/app.module.ts
+++ conversordemoedaslauro/src/app/app.module.ts
@@ -1,26 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { MainComponent } from './components/main/main.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabecalhoComponent,
-    RodapeComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [ AppComponent ]
