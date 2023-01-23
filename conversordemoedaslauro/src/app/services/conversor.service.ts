d37028ec029ffa6b0cbba499ed45d19ec722c297
Lauro Otavio Urbano <74082012+laurourbano@users.noreply.github.com>
2023-01-23 11:46:15 -0300

    Update conversor.service.ts
    conectando api

--- conversordemoedaslauro/src/app/services/conversor.service.ts
+++ conversordemoedaslauro/src/app/services/conversor.service.ts
@@ -1,20 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConversorService {
 
+  BaseUrl: string = 'https://api.exchangerate.host/';
+
   moedaBase?: string;
   moedaOrigem?:string;
   moedaDestino?:string;
   valor?:number;
 
-  BaseUrl: string = 'https://api.exchangerate.host/';
   URLmoedas: string = `${ this.BaseUrl }latest`;
   URL: string = `${ this.BaseUrl }convert?from=${ this.moedaOrigem }&to=${ this.moedaDestino }&amount=${ this.valor }`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  //metodos para buscar informações no backend via api
 }
 
